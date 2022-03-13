f886b134bcc798c1a0029a5c6df1f239f29de75f
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-13 23:39:47 +0300

    set react front end project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './components/Header';
+import NotesList from './pages/NotesList';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <NotesList/>
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/NotesList.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react'
+
+const NotesList = () => {
+    const [notes, setNotes] = useState([]);
+
+    useEffect(() => {
+      first
+    
+      return () => {
+        second
+      }
+    }, [third]);
+
+    const getNotes = asyn
+    
+  return (
+    <div>
+        nots
+    </div>
+  )
+}
+
+export default NotesList
